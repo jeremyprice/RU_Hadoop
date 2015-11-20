7d2c8277f6d340e8c50bd9bc40cee57d3311b763
Jeremy Price <jprice.rhit@gmail.com>
2015-11-20 14:58:31 -0600

    added index generation for all the presentations

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,23 @@ from jinja2 import Environment, FileSystemLoader
 
 template_env = Environment(loader=FileSystemLoader('.'))
 
+presentations = [{'name': 'fundamentals', 'title': 'Hadoop Fundamentals'}]
+                 #{'name': 'operational', 'title': 'Hadoop Operational'}]
+
+def build_all():
+    [build(pres['name']) for pres in presentations]
+    build_index()
+
+def build_index():
+    '''build the index file for all the presentations'''
+    template = template_env.get_template('sources/index.html')
+    rendered_template = template.render(presentations=presentations)
+    with open('presentations/index.html', 'wb') as fh:
+        fh.write(rendered_template)
+
+def operational():
+    build('operational')
+
 def fundamentals():
     build('fundamentals')
 
@@ -14,5 +31,5 @@ def build(pres_name):
         fh.write(rendered_template)
 
 def publish():
-    build()
+    build_all()
     local('ghp-import -p presentations')
